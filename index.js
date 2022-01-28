a7fb40d6d768d7029f8c57d101eed2613f41952a
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-01-28 12:32:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Global Execution Context
+// Memory component i = 5 function x:{....}
+//code component
+
+//phase 1 Memory allocation phase => over
+//phase 2 Code execution phase
+
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
